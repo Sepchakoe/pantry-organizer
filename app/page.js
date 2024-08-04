8d9fe84cbdda7f3dc271d9dc2e0b788535e22788
Koe <koe.w.myint@gmail.com>
2024-08-03 19:06:21 -0700

    Add about page and search function for recipes

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
   const [itemName, setItemName] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
   const [itemQuantity, setItemQuantity] = useState(1);
-
+  const [aboutOpen, setAboutOpen] = useState(false);
 
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'inventory'));
@@ -68,6 +68,9 @@ export default function Home() {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const handleAboutOpen = () => setAboutOpen(true);
+  const handleAboutClose = () => setAboutOpen(false);
+
   const filteredInventory = inventory.filter(item =>
     item.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
@@ -76,7 +79,7 @@ export default function Home() {
     <>
       <Navbar bg="light" variant="light" expand="lg" fixed="top" className="w-100">
         <Container fluid>
-          <Navbar.Brand href="#">
+          <Navbar.Brand onClick={handleAboutOpen} style={{cursor: 'pointer'}}>
             <img
               src="/logo.png"
               width="40"
@@ -94,6 +97,29 @@ export default function Home() {
         </Container>
       </Navbar>
 
+      <Modal show={aboutOpen} onHide={handleAboutClose} className="about-modal">
+        <Modal.Header closeButton>
+          <Modal.Title className="w-100 text-center">Pantry Assistant</Modal.Title>
+        </Modal.Header>
+        
+        <Modal.Body className="d-flex justify-content-center">
+          <img
+            src="/logo.png"
+            width="100"
+            height="100"
+            className="align-center"
+          />
+        </Modal.Body>
+
+        <Modal.Body className="w-100 text-center">
+          <p>A simple and intuitive pantry assistant designed to help you keep track of your ingredients and get personalized recipe suggestions based on your pantry list. You can also search for recipes by entering the ingredients you have on hand.</p>
+        </Modal.Body>
+          
+        <Modal.Footer className="mb-0 text-center py-2 w-100" style={{ color: '#87510f', fontSize: '12px' }}>
+          <p>© 2024 @Koe Myint</p>
+        </Modal.Footer>
+      </Modal>
+
     <Container fluid className="d-flex flex-column align-items-center justify-content-center min-vh-100">
       <h1 style={{ marginTop: '7%'}}>Welcome to Pantry Assistant</h1>
       <img src="/logo.png" alt="Logo" style={{ maxWidth: '200px', height: 'auto' }} />
@@ -105,7 +131,7 @@ export default function Home() {
 
         <Modal.Body>
           <InputGroup className="mb-3">
-            <Form.Control
+            <Form.Control 
               value={itemName}
               onChange={(e) => setItemName(e.target.value)}
               placeholder="Item Name"
@@ -148,7 +174,7 @@ export default function Home() {
       </div>
 
       <Card style={{ width: '60%', marginBottom: '20px'}}>
-        <Card.Header style={{ backgroundColor: '#ADD8E6' }}className="text-center">
+        <Card.Header style={{ backgroundColor: '#ADD8E6'}} className="text-center">
           <h2 className="mt-1 mb-1">Pantry List</h2>
         </Card.Header>
         <Card.Body style={{ maxHeight: '300px', overflowY: 'auto'}}>
@@ -174,13 +200,6 @@ export default function Home() {
         </Card.Body>
       </Card>
     </Container>
-
-    {/* Footer
-    <footer className="fixed-bottom mt-3 bg-light text-center py-2 w-100" style={{ color: 'darkblue', fontSize: '12px' }}>
-      <Container>
-        <p className="mb-0">© 2024 @Koe Myint</p>
-      </Container>
-    </footer> */}
     </>
   );
 }
--- app/recipes/page.js
+++ app/recipes/page.js
@@ -4,14 +4,18 @@
 import { useState, useEffect } from "react";
 import { firestore } from '@/firebase'
 import { collection, getDocs, query } from "firebase/firestore";
-import { Container, Card,  Navbar, Nav, Button, Row, Col } from 'react-bootstrap';
+import { Modal, Container, Card,  Navbar, Nav, Button, Row, Col, Form, InputGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import '/app/globals.css'; 
 
+
 export default function Recipes() {
   const [inventory, setInventory] = useState([]);
   const [recipes, setRecipes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [aboutOpen, setAboutOpen] = useState(false);
 
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'inventory'));
@@ -39,7 +43,21 @@ export default function Recipes() {
     }
   };
 
-  useEffect(() => {
+  const searchRecipes = async (query) => {
+    const apiKey = '5f8ec51c1c264338b06863563bb0c10e';
+    const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&number=5&apiKey=${apiKey}`;
+
+    try {
+      console.log(`Searching recipes for: ${query}`);
+      const response = await axios.get(url);
+      console.log('Search response:', response.data);
+      setSearchResults(response.data.results);
+      } catch (error) {
+        console.error("Error searching recipes:", error);
+      }
+    };
+
+    useEffect(() => {
     updateInventory();
   }, []);
 
@@ -49,14 +67,20 @@ export default function Recipes() {
     }
   }, [inventory]);
 
-  return (
-    <Container fluid className="d-flex flex-column align-items-center justify-content-center min-vh-100">
-      <h1 style={{ marginTop: '7%'}}>Recipe Suggestion</h1>
-      <img src="/recipes.png" alt="Logo" className="mb-2" style={{ maxWidth: '200px', height: 'auto' }} />
+  const handleSearch = () => {
+    if (searchTerm.trim() !== '') {
+      searchRecipes(searchTerm);
+    }
+  };
 
+  const handleAboutOpen = () => setAboutOpen(true);
+  const handleAboutClose = () => setAboutOpen(false);
+
+  return (
+    <>
       <Navbar bg="light" variant="light" expand="lg" fixed="top" className="w-100">
         <Container fluid>
-          <Navbar.Brand href="#">
+          <Navbar.Brand onClick={handleAboutOpen} style={{cursor: 'pointer'}}>
             <img
               src="/logo.png"
               width="40"
@@ -73,6 +97,47 @@ export default function Recipes() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      <Modal show={aboutOpen} onHide={handleAboutClose} className="about-modal">
+        <Modal.Header closeButton>
+          <Modal.Title className="w-100 text-center">Pantry Assistant</Modal.Title>
+        </Modal.Header>
+        
+        <Modal.Body className="d-flex justify-content-center">
+          <img
+            src="/logo.png"
+            width="100"
+            height="100"
+            className="align-center"
+          />
+        </Modal.Body>
+
+        <Modal.Body className="w-100 text-center">
+          <p>A simple and intuitive pantry assistant designed to help you keep track of your ingredients and get personalized recipe suggestions based on your pantry list. You can also search for recipes by entering the ingredients you have on hand.</p>
+        </Modal.Body>
+          
+        <Modal.Footer className="mb-0 text-center py-2 w-100" style={{ color: '#87510f', fontSize: '12px' }}>
+          <p>© 2024 @Koe Myint</p>
+        </Modal.Footer>
+      </Modal>
+
+    <Container fluid className="d-flex flex-column align-items-center justify-content-center min-vh-100">
+      <h1 style={{ marginTop: '7%'}}>Recipe Suggestion</h1>
+      <img src="/recipes.png" alt="Logo" style={{ maxWidth: '200px', height: 'auto' }} />
+
+      <InputGroup className="mb-5 w-50">
+        <Form.Control
+          placeholder = "Search recipes..."
+          value = {searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <Button 
+          variant="outline-primary" 
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </InputGroup>
 
       <Card style={{ width: '60%', marginBottom: '20px'}}>
         <Card.Header style={{ backgroundColor: '#ADD8E6' }}className="text-center">
@@ -134,6 +199,38 @@ export default function Recipes() {
           </Col>
         )}
       </Row>
+
+      {searchResults.length > 0 && (
+        <>
+          <h2 className="mt-5">Recipe with {searchTerm}</h2>
+          <Row className="mt-3 g-4">
+            {searchResults.map(recipe => (
+              <Col key={recipe.id} sm={12} md={6} lg={4} xl={3}>
+                <Card className="h-100 recipe-card">
+                  <Card.Img
+                    variant="top"
+                    src={`https://spoonacular.com/recipeImages/${recipe.id}-312x231.jpg`}
+                    className="recipe-card-img"
+                  />
+                  <Card.Body>
+                    <Card.Title className="recipe-card-title">{recipe.title}</Card.Title>
+                  </Card.Body>
+                  <Card.Footer className="text-center">
+                    <Button
+                      variant="primary"
+                      href={`https://spoonacular.com/recipes/${recipe.title}-${recipe.id}`}
+                      target="_blank"
+                    >
+                      View Recipe
+                    </Button>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </Container>
+    </>
   );
 }
